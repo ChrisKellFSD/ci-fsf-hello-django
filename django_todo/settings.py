db17bea075a77880cd375ebaaf1a3f6fe0be2d18
ChrisKellFSD <chriskellcsn@gmail.com>
2022-11-17 19:07:47 +0000

    Fixed allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-v3-)exzu28iy83ld_u)3)yfz@n^ucnj*jc+_to-)yeaho*pysi
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ckfsd-django-todo-app.herokuapp.com']
 
 
 # Application definition
